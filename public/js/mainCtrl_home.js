dba99ea2853dde34ca641ee23b4bf5dceceb9938
WittawatHamhongsa <pangpondsound@hotmail.com>
2018-03-20 22:34:11 +0700

    commit

--- public/js/mainCtrl_home.js
+++ public/js/mainCtrl_home.js
@@ -3,18 +3,18 @@ angular.module('app', ['jtt_youtube','ui.router',"api_service", 'ngSanitize'])
 
 angular.module('app').config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
-    
+
         $stateProvider
-    
+
             // HOME STATES AND NESTED VIEWS ========================================
             .state('home', {
                 url: '/home',
                 templateUrl : "./partial/home.html"
             })
 
-        
 
-     
+
+
 
 });
 
@@ -22,7 +22,7 @@ angular.module('app').config(function($stateProvider, $urlRouterProvider) {
 angular.module('app')
 
     // super simple service
-    // each function returns a promise object 
+    // each function returns a promise object
     .factory('global_service', ['$rootScope', '$http', function ($rootScope, $http){
         var buf_id ;
     return {
@@ -38,16 +38,16 @@ angular.module('app')
 
     }]);
 angular.module('app').controller('global', function ($scope,$http,api_manage){
-alert();
-    
+  // alert("global ctrl");
+
 });
 
 
 angular.module('app').controller('news_detail', function ($scope,$http,api_manage,global_service){
-   
-  
-    
-   
+
+
+
+
     });
 
 angular.module('app').controller('youtube_box', function ($scope, $filter, $q,$http, youtubeFactory){
@@ -72,37 +72,37 @@ angular.module('app').controller('news', function ($sce,$scope, $filter, $q,$htt
         };
 
     $scope.init = function(){
-        
+
             //  quill_title  quill_detail      quill_title_yo  quill_detail_yo
-           
-      
+
+
             $scope.get_news();
-        
-      
+
+
         }
-    
+
         $scope.news_detail_href= function(id){
 
 
             $scope.init_news_modal(id);
             $('#myModal').modal('show');
-            
 
-  
-         
+
+
+
         }
 
-        
+
 $scope.init_news_modal = function(id){
     alert("new detail = "+id)
-    
+
         let dataObj = {
-            
-            
+
+
             newsID : id,
             readCount:"true",
-       
-                
+
+
               }
                 api_manage.get_news_fromID(dataObj)
                 .success(function(data, status, headers, config) {
@@ -118,32 +118,32 @@ $scope.init_news_modal = function(id){
                     $scope.modal_news  = data.message;
                     console.log('$scope.news_list  =  '+ JSON.stringify($scope.news_list))
                 //    $scope.news_table= new NgTableParams({count: 10 ,  sorting: { resourceName: "desc" }  }, { counts: [10,20, 100], dataset: $scope.news_list });
-                   
+
                   }
-                
+
                 })
                 .error(function(data, status, headers, config) {
                     alert( "failure message: " + JSON.stringify({data: data}) +"ไม่สามารถติดต่อเซิฟเวอร์ได้ ติดต่อแอดมิน");
                     console.log(status+headers);
-                });	
+                });
 
 }
 
 $scope.get_news = function(){
-    
+
     $scope.news_list_1 =[];
     $scope.news_list_2 =[];
     $scope.news_list_3 =[];
     let dataObj = {
-  
-  
+
+
       resourceId : "0",
       departmentId:"0",
       tagId:"0",
       limit:6,
       isPosted:"false",
       isPreview:"true"
-      
+
     }
       api_manage.get_news(dataObj)
       .success(function(data, status, headers, config) {
@@ -164,14 +164,14 @@ $scope.get_news = function(){
           $scope.news_list_3[1] = data.message[5];*/
        //   console.log('$scope.news_list  =  '+ JSON.stringify($scope.news_list))
         //  $scope.news_table= new NgTableParams({count: 10 ,  sorting: { resourceName: "desc" }  }, { counts: [10,20, 100], dataset: $scope.news_list });
-         
+
         }
-      
+
       })
       .error(function(data, status, headers, config) {
           alert( "failure message: " + JSON.stringify({data: data}) +"ไม่สามารถติดต่อเซิฟเวอร์ได้ ติดต่อแอดมิน");
           console.log(status+headers);
-      });	
+      });
     }
 
 
