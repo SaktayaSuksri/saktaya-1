a841e2ba5acfeb55cb9f9f2ec6ebf0a1f6fe7601
saktaya suksri <saktayasuksri@gmail.com>
2018-05-22 01:57:51 +0700

    as

--- routes/mailerAPI.js
+++ routes/mailerAPI.js
@@ -3,67 +3,112 @@
 var express = require('express');
 var router = express.Router();
 
-
+var nodemailer = require('nodemailer');
 var Return_control = require('../controller/return_control.js');
 var ReturnCode = require('../model/returnCode.js');
+
+
+
+
+var fs = require('fs');
+var handlebars = require('handlebars');
 
 // DATABASE SETUP
 
 //มิดเดิ้ลแว อยุ่ข้างบนเสมอ ก่อน get ไว้ทำ log  // เฉพาะ ที่ accessเข้าไฟล์นี้  ดูจากต้นทาง app.ut(/???,....);
 // middleware to use for all requests
 
-var nodemailer = require('nodemailer');
 
+router.post('/deanMailer/', function (request, response) {
+
+    var methodCode = "65";
+
+    console.log(request.body.name)
+var readHTMLFile = function(path, callback) {
+    fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+        if (err) {
+            throw err;
+            callback(err);
+        }
+        else {
+            callback(null, html);
+        }
+    });
+};
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'saktayasuksri@gmail.com',
-    pass: 'sddssdsdsda'
+    user: 'webmaster.kmitl@gmail.com',
+    pass: 'kmitlWebmaster2018'
   }
 });
 
-var mailOptions = {
-  from: 'saktayasuksri@gmail.com',
-  to: 'livelivelivework@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-router.post('/deanMailer/', function (request, response) {
-    // request.body.name
-    // request.body.email 
-    // request.body.phone  
-    // request.body.topic  
-    // request.body.detail
 
-    //mail to admin and investor when admin create  C  S  S+
-    // mail({
-    //     from: "Admin ghb-npl2560 <tum@discoverym.com>", // sender address
-    //     bcc: "", // list of receivers
-    //     subject: "มีชุดคำถาม-ตอบ ใหม่จากระบบ", // Subject line
-    //     text: "✔", // plaintext body
-    //     html: "<h5> สถานะ : " + status + "</h5> สามารถเข้าดูได้ที่ <a href='http://www.ghb-npl2560.com'>ghb-npl2560.com</a>"
+readHTMLFile(__dirname + '/../public/deanmailtemplate.html', function(err, html) {
+ 
+    var template = handlebars.compile(html);
+    var replacements = {
+         name: request.body.name,
+         email:request.body.email ,
+        phone:request.body.phone ,
+topic:request.body.topic  ,
+detail:request.body.detail
+      
 
-    // });
+    };
 
-    mail({
-        from: '"KLEAR" <krystallizer26@gmail.com>', // sender address
-        bcc: 'krystallizer26@gmail.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: 'Hello world?', // plain text body
-        html: "<h5> สถานะ : 555555 </h5> สามารถเข้าดูได้ที่ <a href='http://www.ghb-npl2560.com'>ghb-npl2560.com</a>"
+    var htmlToSend = template(replacements);
+
+    var mailOptions = {
+        from: 'webmaster.kmitl@gmail.com',
+        to: 'livelivelivework@gmail.com',
+        subject: "คณะบดีพบประชาชน: " + request.body.topic,
+        html : htmlToSend
+        
+        
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+            Return_control.responseWithCode(ReturnCode.serviceError + methodCode + "001", error, response);
+        } else {
+            console.log('Email sent: ' + info.response);
+            response.send("<h4 class=;alert alert-success alert-dismissible'>ส่งข้อความเรียบร้อยแล้ว</h4")
+         //   Return_control.responseWithCode(ReturnCode.success, "Sending E-mail, please wait...", response);
+        }
+      });
+    /*var template = handlebars.compile(html);
+    var replacements = {
+         username: "John Doe"
+    };
+    var htmlToSend = template(replacements);
+    var mailOptions = {
+        from: 'my@email.com',
+        to : 'some@email.com',
+        subject : 'test subject',
+        html : htmlToSend
+     };
+    smtpTransport.sendMail(mailOptions, function (error, response) {
+        if (error) {
+            console.log(error);
+            callback(error);
+        }
     });
-    Return_control.responseWithCode(ReturnCode.success, "Email is being sent...", response);
+*/
+
 });
+
+});
+
 
 module.exports = router;
+
+//-----------------------------------------------------------------------------
+
+
+
 
 //-----------------------------------------------------------------------------
