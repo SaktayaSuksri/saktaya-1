a389e904312f3691c254ca75400a800574f3865f
WittawatHamhongsa <pangpondsound@hotmail.com>
2018-05-22 01:13:00 +0700

    commit

--- public/js/mainCtrl_home.js
+++ public/js/mainCtrl_home.js
@@ -345,3 +345,37 @@ $scope.get_news = function(){
 
 
 });
+
+
+angular.module('app').controller('homeHeaderCtrl', function ($sce,$scope, $filter, $q,$http,api_manage,global_service){
+
+    alert("home Header ctrl");
+
+    $(function() {
+        console.log( "ready!" );
+ 
+        
+        alert("home Header ctrl");
+        var parent = document.querySelector('.splitview'),
+            topPanel = parent.querySelector('.top'),
+            handle = parent.querySelector('.handle'),
+            skewHack = 0,
+            delta = 0;
+    
+        // If the parent has .skewed class, set the skewHack var.
+        if (parent.className.indexOf('skewed') != -1) {
+            skewHack = 1000;
+        }
+    
+        parent.addEventListener('mousemove', function(event) {
+            // Get the delta between the mouse position and center point.
+            delta = (event.clientX - window.innerWidth / 2) * 0.5;
+    
+            // Move the handle.
+            handle.style.left = event.clientX + delta + 'px';
+    
+            // Adjust the top panel width.
+            topPanel.style.width = event.clientX + skewHack + delta + 'px';
+        });
+    });
+});
