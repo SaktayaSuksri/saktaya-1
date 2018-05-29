1d8b458a6a6fe906ab71e7c5b2440772a1e8e00f
WittawatHamhongsa <pangpondsound@hotmail.com>
2018-05-29 21:15:33 +0700

    commit

--- config/config.js
+++ config/config.js
@@ -3,7 +3,8 @@
 
 module.exports = {
 	port : 8080,
-	dbUrl: 'mongodb://sciweb_admin1:sciwebkmitl2018@localhost:27017/sciweb',
+	 //dbUrl: 'mongodb://testUser:testPass@ds111568.mlab.com:11568/scienceweb', 
+	dbUrl: 'mongodb://sciweb_admin1:sciwebkmitl2018@localhost:27017/sciweb', 
 	sessionSecret : 'ai',
 	consumerKey: 'my_twitter_consumer_key',
     consumerSecret: 'my_twitter_consumer_key'
