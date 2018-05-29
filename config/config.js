e20c4df7a8417474329bd035b29f6939a12deb6a
WittawatHamhongsa <pangpondsound@hotmail.com>
2018-05-29 20:47:52 +0700

    Pond edit mongo db

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
 	port : 8080,
-	dbUrl: 'mongodb://testUser:testPass@ds111568.mlab.com:11568/scienceweb',
+	dbUrl: 'mongodb://sciweb_admin1:sciwebkmitl2018@localhost:27017/sciweb',
 	sessionSecret : 'ai',
 	consumerKey: 'my_twitter_consumer_key',
     consumerSecret: 'my_twitter_consumer_key'
